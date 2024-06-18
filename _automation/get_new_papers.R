8b59a392e61e5e1f980aefddf002b6684eedced3
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2024-06-18 14:56:14 +0100

    update paper machine and papers

--- _automation/get_new_papers.R
+++ _automation/get_new_papers.R
@@ -1,22 +1,40 @@
-`%>%` <- magrittr::`%>%`
-
 get_new_papers_orcid <- function(orcid, from_date) {
-  
-  rcrossref::cr_works(filter = list(orcid = orcid,
-                                    from_deposit_date = from_date,
-                                    type = "journal-article")) %>%
+  refs <- rcrossref::cr_works(filter = list(
+    orcid = orcid,
+    from_deposit_date = from_date
+  )) |>
     purrr::pluck("data")
-  
+
+  if (nrow(refs) > 0) {
+    refs <- refs |>
+      dplyr::mutate(
+        container.title = dplyr::if_else(
+          publisher == "Cold Spring Harbor Laboratory",
+          "medRxiv",
+          container.title
+        ),
+        type = dplyr::if_else(
+          publisher == "Cold Spring Harbor Laboratory",
+          "journal-article",
+          type
+        )
+      ) |>
+      dplyr::filter(type == "journal-article")
+    existing_papers <- dois_from_bib("_data/papers.bib")
+    refs <- refs |>
+      dplyr::filter(!(doi %in% existing_papers))
+  }
+  return(refs)
 }
 
 get_new_papers_team <- function(from_date) {
   
-  recent_papers <- fs::dir_ls("_data/team", regexp = "\\w+\\-\\w+\\.yml") %>%
-    purrr::map(yaml::read_yaml) %>%
-    purrr::keep(function(x) x[["current-member"]]) %>%
-    purrr::map("orcid") %>%
-    purrr::keep(~ !is.null(.x)) %>%
-    purrr::map_dfr(get_new_papers_orcid, from_date = from_date) %>%
+  recent_papers <- fs::dir_ls("_data/team", regexp = "\\w+\\-\\w+\\.yml") |>
+    purrr::map(yaml::read_yaml) |>
+    purrr::keep(function(x) x[["current-member"]]) |>
+    purrr::map("orcid") |>
+    purrr::keep(~ !is.null(.x)) |>
+    purrr::map_dfr(get_new_papers_orcid, from_date = from_date) |>
     dplyr::distinct()
   
   return(recent_papers)
@@ -24,14 +42,14 @@ get_new_papers_team <- function(from_date) {
 }
 
 create_bibentries <- function(papers) {
-  papers %>%
+  papers |>
     dplyr::mutate(
       author = purrr::map(author, ~ dplyr::filter(.x, !is.na(given) &!is.na(family))),
       author = purrr::map(author, ~ paste(.x$given, .x$family)),
       author = purrr::map(author, as.person),
       year = format(lubridate::parse_date_time(issued, c("Y-m-d", "Y-m", "Y")), "%Y")
-    ) %>%
-    dplyr::rowwise() %>%
+    ) |>
+    dplyr::rowwise() |>
     dplyr::mutate(
       bib = list(bibentry(
         "Article", 
@@ -42,7 +60,13 @@ create_bibentries <- function(papers) {
         doi = doi,
         key = ids::adjective_animal()
       ))
-    ) %>%
+    ) |>
     dplyr::pull(bib)
   
+}
+
+dois_from_bib <- function(bib) {
+  bib |>
+    bibtex::read.bib() |>
+    purrr::map_chr(~ .x$doi)
 }
